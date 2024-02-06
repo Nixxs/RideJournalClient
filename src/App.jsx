da12be65a4acaffe65f4225abf8dd4f8357967bf
Nicholas Chai <nicholas.chai@outlook.com>
2024-02-06 19:43:07 +0800

    added an about page so we can have a page that explains what what journal is and what its meant to be

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts";
 import Home from "./pages/Home";
 import Vehicles from "./pages/Vehicles";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
   {
@@ -9,7 +10,8 @@ const router = createBrowserRouter([
     element:<Layout />,
     children: [
       {path:"/",element:<Home />},
-      {path:"/vehicles",element:<Vehicles />}
+      {path:"/vehicles",element:<Vehicles />},
+      {path:"/about",element:<About />}
     ]
   }
 ]);
