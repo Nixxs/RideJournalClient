0a52fad35a8588aa2af1587e7bf1bcbc40f7eed5
Nicholas Chai <nicholas.chai@outlook.com>
2024-02-17 17:31:01 +0800

    had to change the way user data was being retrieved so that we werent doing another fetch to the database to get user information.

--- src/features/EventManager/EventCard.jsx
+++ src/features/EventManager/EventCard.jsx
@@ -19,17 +19,20 @@ import UserAvatar from "../../components/UserAvatar";
 import Tooltip from "@mui/material/Tooltip";
 import LikeEvent from "../../components/LikeEvent";
 import Comments from "../../components/Comments";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
 
 function EventCard({ eventId }) {
+  const theme = useTheme();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [state, dispatch] = useReducer(eventDetailsReducer, initialState);
 
   // get the event details
   useEffect(() => {
     fetch(
-      `${import.meta.env.VITE_REACT_APP_SERVER_URL}/api/events/${
-        eventId
-      }/include`
+      `${
+        import.meta.env.VITE_REACT_APP_SERVER_URL
+      }/api/events/${eventId}/include`
     )
       .then((response) => response.json())
       .then((eventData) => {
@@ -92,166 +95,177 @@ function EventCard({ eventId }) {
       default:
         return "💢";
     }
-  }
+  };
 
-  return (
-    (state.eventDetails === null) ? ( <div>Loading...</div> ) : (
-      <Card
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "50vh",
-          maxWidth: "60vw",
-          minWidth: 300,
-          m: 2,
-          padding: 0,
-          // If you want the image to take full width on small screens
-          "& > div": { width: "100%" },
-        }}
-      >
-        <div style={{ height: "100%", position: "relative" }}>
-          <CardContent
-            sx={{
-              width: "100%",
-              margin: 0,
-              paddingBottom: 0,
-              display: "flex",
-              flexDirection: "row",
-            }}
-          >
-            <Box sx={{flexGrow: 0, marginRight: 2, marginTop: 0.7}}>
-              <UserAvatar userId={state.eventDetails.Vehicle.userId}/>
-            </Box>
-            <Box sx={{flexGrow: 1}}>
-              <Typography variant="h6" color="text.secondary">
-                <Tooltip title={state.eventDetails.type} arrow> 
-                    <span>{eventTypeIcon(state.eventDetails.type)}</span>
-                </Tooltip>
-                {" "}
-                {state.eventDetails.title}
-              </Typography>
-              <Typography variant="body2"  color="text.secondary">
-                {state.eventDetails.createdAt}
-              </Typography>
-            </Box>
-            <Box sx={{flexGrow: 0, marginTop: 0.5}}>
-              <Typography variant="body2"  color="text.secondary" align="right">
-                {state.eventDetails.Vehicle.name} - {state.eventDetails.Vehicle.location}
-                <br />
-                {state.eventDetails.Vehicle.year} {" "} {state.eventDetails.Vehicle.make} 
-                {" "} {state.eventDetails.Vehicle.model}
-                <br />
-                {state.eventDetails.odometer} kms
-              </Typography>
-            </Box>
-          </CardContent>
-          <ImageList
-            sx={{
-              width: "100%",
-              height: "100%",
-              margin: 0,
-            }}
-            rowHeight={450}
-            cols={1}
-          >
-            <ImageListItem key={state.eventDetails.Images[currentIndex].id}>
-              <img
-                src={`${import.meta.env.VITE_REACT_APP_SERVER_URL}/images/${
-                  state.eventDetails.Images[currentIndex].image
-                }?w=500&h=450&fit=crop&auto=format`}
-                srcSet={`${import.meta.env.VITE_REACT_APP_SERVER_URL}/images/${
-                  state.eventDetails.Images[currentIndex].image
-                }?w=500&h=450&fit=crop&auto=format&dpr=2 2x`}
-                alt={`${state.eventDetails.title}-${state.eventDetails.Images[currentIndex].id}`}
-                loading="lazy"
-                className="fade-in"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-            </ImageListItem>
-          </ImageList>
-          {state.eventDetails.Images.length > 1 && (
-            <>
-              <IconButton
-                sx={{
-                  position: "absolute",
-                  top: "50%",
-                  left: 0,
-                  transform: "translateY(-50%)",
-                  color: "white",
-                  bgcolor: "rgba(0, 0, 0, 0.5)",
-                  "&:hover": {
-                    bgcolor: "rgba(0, 0, 0, 0.7)",
-                  },
-                }}
-                onClick={handlePrevious}
-              >
-                <ArrowBackIosIcon />
-              </IconButton>
-              <IconButton
-                sx={{
-                  position: "absolute",
-                  top: "50%",
-                  right: 0,
-                  transform: "translateY(-50%)",
-                  color: "white",
-                  bgcolor: "rgba(0, 0, 0, 0.5)",
-                  "&:hover": {
-                    bgcolor: "rgba(0, 0, 0, 0.7)",
-                  },
-                }}
-                onClick={handleNext}
-              >
-                <ArrowForwardIosIcon />
-              </IconButton>
-            </>
-          )}
-        </div>
+  return state.eventDetails === null ? (
+    <div>Loading...</div>
+  ) : (
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "50vh",
+        maxWidth: "900px",
+        minWidth: 300,
+        m: 2,
+        padding: 0,
+        // If you want the image to take full width on small screens
+        "& > div": { width: "100%" },
+      }}
+    >
+      <div style={{ height: "100%", position: "relative" }}>
         <CardContent
           sx={{
             width: "100%",
-            paddingTop: 1,
+            margin: 0,
+            paddingBottom: 0,
+            display: "flex",
+            flexDirection: "row",
           }}
         >
-          {/* TODO: put in like and comment Buttons */}
-          <Box sx={{
+          <Box sx={{ flexGrow: 0, marginRight: 2, marginTop: 0.7 }}>
+            <UserAvatar userData={state.eventDetails.User} />
+          </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" color="text.secondary">
+              <Tooltip title={state.eventDetails.type} arrow>
+                <span>{eventTypeIcon(state.eventDetails.type)}</span>
+              </Tooltip>{" "}
+              {state.eventDetails.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {state.eventDetails.createdAt}
+            </Typography>
+          </Box>
+          <Box sx={{ flexGrow: 0, marginTop: 0.5 }}>
+            <Typography variant="body2" color="text.secondary" align="right">
+              {state.eventDetails.Vehicle.name} -{" "}
+              {state.eventDetails.Vehicle.location}
+              <br />
+              {state.eventDetails.Vehicle.year}{" "}
+              {state.eventDetails.Vehicle.make}{" "}
+              {state.eventDetails.Vehicle.model}
+              <br />
+              {state.eventDetails.odometer} kms
+            </Typography>
+          </Box>
+        </CardContent>
+        <ImageList
+          sx={{
+            width: "100%",
+            height: "100%",
+            margin: 0,
+          }}
+          rowHeight={450}
+          cols={1}
+        >
+          <ImageListItem key={state.eventDetails.Images[currentIndex].id}>
+            <img
+              src={`${import.meta.env.VITE_REACT_APP_SERVER_URL}/images/${
+                state.eventDetails.Images[currentIndex].image
+              }?w=500&h=450&fit=crop&auto=format`}
+              srcSet={`${import.meta.env.VITE_REACT_APP_SERVER_URL}/images/${
+                state.eventDetails.Images[currentIndex].image
+              }?w=500&h=450&fit=crop&auto=format&dpr=2 2x`}
+              alt={`${state.eventDetails.title}-${state.eventDetails.Images[currentIndex].id}`}
+              loading="lazy"
+              className="fade-in"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </ImageListItem>
+        </ImageList>
+        {state.eventDetails.Images.length > 1 && (
+          <>
+            <IconButton
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: 0,
+                transform: "translateY(-50%)",
+                color: "white",
+                bgcolor: "rgba(0, 0, 0, 0.5)",
+                "&:hover": {
+                  bgcolor: "rgba(0, 0, 0, 0.7)",
+                },
+              }}
+              onClick={handlePrevious}
+            >
+              <ArrowBackIosIcon />
+            </IconButton>
+            <IconButton
+              sx={{
+                position: "absolute",
+                top: "50%",
+                right: 0,
+                transform: "translateY(-50%)",
+                color: "white",
+                bgcolor: "rgba(0, 0, 0, 0.5)",
+                "&:hover": {
+                  bgcolor: "rgba(0, 0, 0, 0.7)",
+                },
+              }}
+              onClick={handleNext}
+            >
+              <ArrowForwardIosIcon />
+            </IconButton>
+          </>
+        )}
+      </div>
+      <CardContent
+        sx={{
+          width: "100%",
+          paddingTop: 1,
+        }}
+      >
+        {/* TODO: put in like and comment Buttons */}
+        <Box
+          sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "flex-start",
             alignItems: "center",
             marginTop: 1,
-          }}>
-            <Box>
-              <LikeEvent eventId={state.eventDetails.id} likeData={state.eventDetails.Likes}/>
-            </Box>
-            <Box sx={{marginLeft:1}}>
-              <Comments eventId={state.eventDetails.id} commentData={state.eventDetails.Comments} 
-                sx={{marginLeft: "30px"}}
-              />
-            </Box>
+          }}
+        >
+          <Box>
+            <LikeEvent
+              eventId={state.eventDetails.id}
+              likeData={state.eventDetails.Likes}
+            />
           </Box>
+          <Box sx={{ marginLeft: 1 }}>
+            <Comments
+              eventId={state.eventDetails.id}
+              commentData={state.eventDetails.Comments}
+              sx={{ marginLeft: "30px" }}
+            />
+          </Box>
+        </Box>
 
-          <Typography
-            sx={{ marginTop: 1 }}
-            variant="body1"
-            color="text.secondary"
-          >
-            {state.eventDetails.detail}
-          </Typography>
-          
-          <CardActions>
-            <NavLink to={`/vehicles/${state.eventDetails.Vehicle.id}`}>
-              <Button sx={{marginLeft: -1}} size="small" variant="outlined">View Timeline</Button>
-            </NavLink>
-          </CardActions>
+        <Typography sx={{ marginTop: 1.5, marginBlock: 1 }} variant="body1" color="text.secondary">
+          <Link to={`/profile/${state.eventDetails.User.id}`} style={{ 
+            textDecoration: "none", 
+            color: theme.palette.text.secondary,
+            fontWeight: "bold"
+          }}>
+            {state.eventDetails.User.name}
+          </Link>
+          : {state.eventDetails.detail}
+        </Typography>
 
-        </CardContent>
-      </Card>
-    )
-
+        <CardActions>
+          <NavLink to={`/vehicles/${state.eventDetails.Vehicle.id}`}>
+            <Button sx={{ marginLeft: -1 }} size="small" variant="outlined">
+              View Timeline
+            </Button>
+          </NavLink>
+        </CardActions>
+      </CardContent>
+    </Card>
   );
 }
 
